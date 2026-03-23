eee732c3d3580314b6655f6668b8ba16e5b151a3
Alina <alina.skolkovo@gmail.com>
2026-03-23 15:49:41 +0300

    доработка после замечаний

--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -1,19 +1,19 @@
 package model;
 
+import model.constants.Colour;
 import model.constants.Discount;
 
 public class Apple extends Food {
 protected String colour;
 
 public Apple (int amount, double price,String colour){
-    super(amount,price);
+    super(amount,price,true);
     this.colour = colour;
-    this.isVegetarian = true;
 }
 
     @java.lang.Override
     public double getDiscount() {
-        if (Colour.Red.equals(colour)) {
+        if (Colour.RED.equals(colour)) {
             return Discount.DISCOUNT_FOR_RED;
         }
         return Discount.DISCOUNT_NOT;
--- src/main/java/model/Meat.java
+++ src/main/java/model/Meat.java
@@ -3,7 +3,7 @@ package model;
 public class Meat extends Food {
 
     public Meat (int amount, double price) {
-        super(amount,price);
+        super(amount,price,false);
         this.isVegetarian = false;
     }
 }
